9d0f54bc159fd8b49f367fec98ad73dc52c92c03
Rue The Ghetto <ru_ghetto@rubyghetto.com>
2009-01-21 00:05:45 -0800

    more test setup

--- spec/rear_views_spec.rb
+++ spec/rear_views_spec.rb
@@ -1,8 +1,26 @@
 require File.dirname(__FILE__) + '/spec_helper'
+require 'rubygems'
+require 'merb-core'
+require 'merb-gen'
+$TESTING = true
+require File.dirname(__FILE__) + '/../lib/generators/rear_views_generator'
 
 describe "rear_views" do
-  it "spec tests should be setup correctly" do
-    true.should == true
-  end
+  before(:all) do
+    @path = File.dirname(__FILE__) + "/../"
+  end  
+  
+  
+  describe 'test setup' do
+    it "should have all dependencies and rake tasks needed to run" do
+      true.should == true
+    end
+    
+    it 'should be able to instantiate a RearViewGenerator' do
+      lambda do
+        Merb::Generators::RearViewsGenerator.new( @path, {}, 'Test' )
+      end.should_not raise_error  
+    end  
+  end  
    
 end
